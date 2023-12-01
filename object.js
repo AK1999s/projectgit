33af2eb52782f8c0f71a437233662eb4de1cd393
Amrendra kumar <142972274+AK1999s@users.noreply.github.com>
2023-12-01 10:11:58 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,110 @@
+/*var car_manufacturer ="Audi";
+var car_model = "Q7";
+var price = 50000;
+function allwheeldrive() {
+    document.write("this car comes with Quattro all wheel drive");
+
+}
+allwheeldrive() OBJECT METHOD /*
+
+/*var person = { OBJECT METHOD 
+    firstname : "Amrendra",
+    lastname : "kumar",
+    id : 5556,
+    fullName : function() {
+        return this.firstname + " " + this.lastname;
+
+    }
+};
+document.write(person.fullName());*/
+  /* Adding a method in object var person = {
+    firstname : "Amrendra",
+    lastname : "Kumar",
+    id : 5556
+    
+ };
+ person.name = function () {
+        return (this.firstname + " " + this.lastname).toUpperCase();
+ };
+ document.write("my fathe name is" + person.name());*/
+ 
+/* var person = {
+    firstname : "Ram",
+    lastname : "kumar",
+    id : 44145,
+
+ };
+ var txt="";
+ for(let x in person) {
+    txt+= person[x] +" ";
+
+ };
+ document.write(txt);*/
+
+  /* JSON Stringify var person= {
+    firstname : "Amrendra",
+    lastname : "Kumar",
+    today : new Date()
+ };
+ var myString = JSON.stringify(person);
+ document.write(myString);*/
+ 
+ /*var car = {
+    car_manufacturer : "Audi",
+    car_model : 1997,
+    language : "en",
+    get lang() {
+        return this.language;
+    }
+ };
+ document.write(car.lang);*/
+
+
+ /*function person(first,last,age,eye) {
+    this.firstName =first;
+    this.lastName = last;
+    this.age = age;
+    this.eyecolor = eye;
+
+ }
+ var myFather = new person("john","doe",50,"blue");
+ document.write("my father is" + myFather.age +".");*/
+
+ /*var name = "Amrendra";
+ var text = "";
+ for(var x of name) {
+    text += x + "<br>";
+
+ }
+ document.write(text);*/
+
+ var letters = new set();
+var a ="a";
+var b = "b";
+var c = "c";
+
+letters.add(a);
+letters.add(b);
+letters.add(c);
+
+document.write(letters.size);
+
+ 
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
